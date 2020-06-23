7c71c860683f008a4edbb63fc885aa577a1adf70
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-24 02:01:31 +0530

    Added: CSV to Json program

--- src/main/java/com/bridgelabz/com/OpenCSV/CSVUSER.java
+++ src/main/java/com/bridgelabz/com/OpenCSV/CSVUSER.java
@@ -1,35 +1,23 @@
 package com.bridgelabz.com.OpenCSV;
 import com.opencsv.bean.CsvBindByName;
-class CSVUser {
-      @CsvBindByName
-      private String name;
+public class CSVUSER {
+            @CsvBindByName
+            private String name;
 
-      @CsvBindByName(column = "email", required = true)
-      private String email;
+            @CsvBindByName(column = "email", required = true)
+            private String email;
 
-      @CsvBindByName(column = "phone")
-      private String phoneNo;
-      @CsvBindByName
-      private String country;
-      public CSVUser(String s, String s1, String s2, String india) {
-            this.name = s;
-            this.email = s1;
-            this.phoneNo = s2;
-            this.country = india;
-      }
-      public String getName() {
-            return this.name;
-      }
-      public String getEmail() {
-            return this.email;
-      }
-      public String getPhoneNo() {
-            return this.phoneNo;
-      }
-      public String getCountry() {
-            return this.country;
-      }
-      public String toString(){
-            return this.name+" "+this.email+" "+this.phoneNo+" "+this.country;
-      }
+            @CsvBindByName(column = "phone")
+            private String phoneNo;
+            @CsvBindByName
+            private String country;
+            @Override
+            public String toString(){
+                  return "CSVUser{" +
+                          "name='" + name + '\'' +
+                          ", email='" + email + '\'' +
+                          ", phoneNo='" + phoneNo + '\'' +
+                          ", country='" + country + '\'' +
+                          '}';
+            }
 }
